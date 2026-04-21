b4404f0220e2ba5fd42ee5170a66449371d0cb49
daejung2613-blip <daejung2613@dge.go.kr>
2026-04-21 09:53:22 +0900

    Add files via upload

---
+++ S0309.java
@@ -0,0 +1,10 @@
+package jump2java;
+import java.util.Arrays;
+import java.util.HashSet;
+
+public class S0309 {
+	public static void main(String[] args) {
+		HashSet<String> set = new HashSet<>(Arrays.asList("H", "e", "l", "l", "o"));
+		System.out.println(set);
+	}
+}
